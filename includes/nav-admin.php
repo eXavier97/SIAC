d7c6988e671c33fc8b5ac9f1abecc92486e62aed
yvargas98 <44735624+yvargas98@users.noreply.github.com>
2018-11-23 04:49:09 -0600

    Añadido el formulario de los Datos Generales del Participante

--- includes/nav-admin.php
+++ includes/nav-admin.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" style="height:100px;">
     <a href="admin.php" class="navbar-brand">
-        <img src="images/logoOficial.jpeg" style="width: 30px; height: 30px; border-radius: 20%" alt="logo">
+        <img src="images/logoOficial.jpeg" style="width: 70px; height: 70px; border-radius: 20%" alt="logo">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
                 <a class="nav-link" href="admin.php">Inicio <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" style="color:white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Registrar
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
